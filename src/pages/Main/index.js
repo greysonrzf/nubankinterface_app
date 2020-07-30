d6fc824c78e40f6adf2f451132aaaf825caba0e7
Greyson Rampazo <greysonf@gmail.com>
2020-07-30 10:54:07 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import Header from '~/components/Header'
+import Tabs from '~/components/Tabs'
+
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 1.650.110,90</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20.000,00 recebida de Camila Soares de Oliveira hoje às 10:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
+    </Container>
+  );
+}
+
+export default Main;
